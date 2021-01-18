445594f8030cd2ce8d4466887d26d0c62906ba0e
VenkataPenugonda <65691075+VenkataPenugonda@users.noreply.github.com>
2021-01-18 13:02:25 -0500

    Add files via upload

---
+++ chapter08-project01.js
@@ -0,0 +1,42 @@
+/* add loop and other code here ... in this simple exercise we are not
+   going to concern ourselves with minimizing globals, etc */
+class Book{
+   constructor(fileName,title,quantity,price,total){
+      this.fileName=fileName;
+      this.title=title;
+      this.quantity=quantity;
+      this.price=price;
+      this.total=total;
+   }
+}
+var filenames = ["106020.jpg", "116010.jpg", "120010.jpg"];
+var titles = ["Girl with a Pearl Earring", "Artist Holding a Thistle", "Portrait of Eleanor of Toledo"];
+var quantities = [3, 1, 2];
+var prices = [80, 125, 75];
+
+var doms=[];
+for(var i=0;i<filenames.length;i++){
+doms.push(new Book(filenames[i],titles[i],quantities[i],prices[i],quantities[i]*prices[i]));
+}
+var Subtotal=0;
+for(var j=0;j<doms.length;j++){
+   document.write('<tr>'); 
+   document.write('<td>' + '<img src=images/' + doms[j].fileName + ' />' + '</td>');
+   document.write('<td>' + doms[j].title + '</td>');
+   document.write('<td>' + doms[j].quantity + '</td>');
+   document.write('<td>' +"$"+ doms[j].price + '</td>');
+   document.write('<td>' +"$"+doms[j].total + '</td>');
+   document.write('</tr>');
+   subTotal+=doms[j].total;
+}
+var tax=subTotal*0.10;
+var shippingPrice=0;
+if(subTotal>1000){
+   shippingPrice=40;
+}
+var finalPrice = subTotal + tax + shippingPrice;
+
+document.write("<tr class='totals'><td colspan='4'>Subtotal</td><td>$"+subTotal+"</td></tr>");
+document.write("<tr class='totals'><td colspan='4'>Tax</td><td>$"+tax+"</td></tr>");
+document.write("<tr class='totals'><td colspan='4'>Shipping</td><td>$"+shippingPrice+"</td></tr>");
+document.write("<tr class='totals focus'><td colspan='4'>Grand Total</td><td>$"+finalPrice+"</td></tr>")
---
+++ data.js
@@ -0,0 +1,20 @@
+/* here are the arrays used for project 1 */
+
+var filenames = ["106020.jpg", "116010.jpg", "120010.jpg"];
+var titles = ["Girl with a Pearl Earring", "Artist Holding a Thistle", "Portrait of Eleanor of Toledo"];
+var quantities = [3, 1, 2];
+var prices = [80, 125, 75];
+
+// var doms=[];
+// for(var i=0;i<filenames.length;i++){
+// doms.push(new Book(filenames[i],titles[i],quantities[i],prices[i],quantities[i]*prices[i]));
+// }
+
+/* 
+   NOTE: parallel arrays are not an ideal way to represent this data.
+         We have done this to simplify this exercise.
+         
+         A better approach would be to turn these parallel arrays
+         into an array of cart item objects. Objects are used in
+         projects 2 and 3.
+*/         
