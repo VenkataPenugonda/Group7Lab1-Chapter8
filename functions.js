1d02ea7eb9a0c3efae7780bc660a74ec6ea64301
akhil-html <65633373+akhil-html@users.noreply.github.com>
2021-01-18 13:17:38 -0500

    Add files via upload

--- functions.js
+++ functions.js
@@ -2,3 +2,15 @@
 
 
         
+
+var calculateTotal=(quantity,price)= {return quantity*price};
+console.log(calculateTotal());
+var outputCartRow = (file,title,quantity,price,total) {};
+var outputCartRow = (file,title,quantity,price,total) = {doc.write('<tr>')
+doc.write('<td>' + '<img src='+file+ />' + '</td'>);
+doc.write('<td>' + title+ '</td/>');
+doc.write('<td>' + quantity+ '</td/>');
+doc.write('<td>' + price.toFixed(2)+ '</td/>');
+// toFixed(2) to display 2 decimal places //
+doc.write('<td>' + total.toFixed(2)+ '</td/>');
+doc.write('<tr>')};
