2ddfc2dd7b3c3412b2d207bf434925091cec289e
iedirisi <iedirisi@confederationcollege.ca>
2021-01-28 22:02:14 -0500

    Everything

---
+++ Project 1/chapter08-project01.js
@@ -0,0 +1,13 @@
+/* add loop and other code here ... in this simple exercise we are not
+   going to concern ourselves with minimizing globals, etc */
+
+var filenames = ["106020.jpg", "116010.jpg", "120010.jpg"];
+for(row=0;row<3;row++)
+total = calculateTotal(quantity[row]*prices[row]);
+outputCartRow(filenames[row],titles[row],quantities[row],prices[row],total[row]);
+subTotal=total+price;
+taxamount=subtotal*0.10;
+//10% as tax amount//
+shipping price=0;subTotal<=1000;shipping price=40;
+grandTotal=subTotal+shipping price+taxamount;
+<td>$+finalPrice.toFixed(2)+"</td>
